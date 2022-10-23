068039d6346d4de0b4fe25c7bf89ec5a8291830a
Rafael Fontana Becker Gois <87453597+RafaGois@users.noreply.github.com>
2022-10-23 18:37:13 -0300

    sketch 03

--- sketch-02/sketch-02.js
+++ sketch-02/sketch-02.js
@@ -1,11 +1,10 @@
 const canvasSketch = require('canvas-sketch');
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+
 
 const settings = {
   dimensions: [ 1080, 1080 ]
-};
-
-const degToRad = (degrees) => {
-  return degrees / 180 * Math.PI;
 };
 
 const sketch = () => {
@@ -16,25 +15,45 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
     const w = width * 0.01;
     const h = height * 0.1;
+  
+    let x,y;
 
-    const num = 12;
+    const num = 40;
+    const radius = width * 0.3
 
     for (let i = 0; i < num; i++) {
 
-      const slice = degToRad( 360 / num);
+      const slice = math.degToRad( 360 / num);
       const angle = slice * i;
 
+      x = radius * Math.sin(angle);
+      y = radius * Math.cos(angle);
+
       context.save();
+      context.translate(cx,cy);
       context.translate(x,y);
-      context.rotate(angle);
+      context.rotate(-angle);
+      context.scale(random.range(0.1,2),random.range(0.2,0.5));
 
       context.beginPath();
-      context.rect(-w * 0.5,-h * 0.5,w,h);
+      context.rect(-w * 0.5,random.range(0, -h * 0.5),w,h);
       context.fill();
+      context.restore();
+
+
+      context.save();
+      context.translate(cx,cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5,20);
+
+      context.beginPath();
+      context.arc(0,0,radius * random.range(0.7,1.3), slice * random.range(1, -8), slice * random.range(1,5));
+      context.stroke();
       context.restore();
     }
   };
